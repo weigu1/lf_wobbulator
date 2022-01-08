2a4f20b2ad4d2e6dbeb77665183a7087bd853c19
jean-claudeF <jean-claude.feltes@education.lu>
2022-01-08 16:00:41 +0100

    Add files via upload

--- PCpython/test_wobbulator.py
+++ PCpython/test_wobbulator.py
@@ -11,27 +11,45 @@ def set_frequency(f):
     cmd_to_pico(ser, "wave.set_freq(" + str(f) + ")" )
     
     
-
-    
-if __name__ == "__main__":  
-    
+def init_modules():
     ser = open_pico("lf_wobbulator")
     time.sleep(0.1)
     init_pico(ser)
     time.sleep(0.1)
     cmd_to_pico(ser, "from ad9833  import *")
     time.sleep(0.2)
+    
     cmd_to_pico(ser, "wave = create_default_wave()")  
     time.sleep(0.1)
+    return ser
+
+def close_pico():
+    reset_pico(ser)
+    ser.close()
+
     
+if __name__ == "__main__":  
+    
+    ser = init_modules()
+    
+    
+    ''' 
     for i in range(1,3):
         f = i * 100
         set_frequency(f)
         time.sleep(0.01)
+    '''
+    #set_frequency(1000)
     
-    set_frequency(1000)
-    reset_pico(ser)
-    ser.close()
+    while True:
+        f = float(input("Frequency/Hz: "))
+        if f == 0:
+            break
+        set_frequency(f)
+    
+    
+    
+    close_pico()
     
 """
 Micropy commands:
